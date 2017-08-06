82b1177b8123739597ba7f1e2895854252e6ebef
Ricardo Ruiz <rjruizfdez@gmail.com>
2017-08-06 14:44:52 +0200

    Added functionalities for the first time

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # instagram-user-id
 Get the user ID of any user in instagram
+
+
+## Usage
+
+python main.py USERNAME
+
+
+## Example
+
+python main.py ricardojoserf
---
+++ main.py
@@ -0,0 +1,26 @@
+import sys, json, requests, os
+
+
+def get_id(username):
+	url="https://www.instagram.com/"+username+"/?__a=1"
+	response = requests.get(url)
+	respJSON = response.json()
+	try:
+		user_id = str(respJSON['user'].get("id"))
+		return user_id
+	except:
+		print(respJSON)
+		return "."
+
+def main():
+
+	if len(sys.argv) < 2:
+		print("Usage: \npython main.py USERNAME")
+		return
+
+	user_id = get_id(sys.argv[1])
+
+	print("ID: "+user_id)
+
+if __name__ == "__main__":
+    main()	
